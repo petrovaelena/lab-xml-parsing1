02d6cb425b12288df92bb6d11895337eff582f6c
petrovaelena <lena517594@yandex.ru>
2015-11-25 21:22:56 +0300

    Update xmlParser.js

--- xmlParser.js
+++ xmlParser.js
@@ -94,7 +94,7 @@ function getFieldType(type, value) {
 function CreateRow() {
 
     var table = document.getElementById('id_Table');
-    var newRow = table.insertRow(1);
+    var newRow = table.insertRow(9);
     for (var i = 0; i < CELL_Number; i++) {
         var newColumn = newRow.insertCell(i);
         newColumn.innerHTML = "<input type=\"text\" value=\"\" />";

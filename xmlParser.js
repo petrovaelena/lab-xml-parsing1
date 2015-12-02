778ad19255761c4e95cd1cbcdc769bd66f5c6734
petrovaelena <lena517594@yandex.ru>
2015-12-02 10:45:12 +0300

    Update xmlParser.js

--- xmlParser.js
+++ xmlParser.js
@@ -1,293 +1,289 @@
-var CELL_Number = 4;
-function validator(input,type) {
-    var value = input.value;
-    
-    //onkeydown
-    if (isValueValid(input, type) === false) {
-        input.setAttribute('id', 'wrong');
-        return false;
-    }
-    else
-        input.setAttribute('id', 'complete');
+var cnstCNT_CELL = 4;
+var MAX_INT_32 =  2147483647;
+var MAX_CNT_INT =  10;
 
-    if (type === 'System.Int32') {
-       var Reg = new RegExp("(^([+-]?)([1-9]+?)[0-9]*)|^0$");
-      // var Reg = new RegExp("^([+-]?)[0-9]+$");
-        if (Reg.test(value)) {
-            input.setAttribute('id', 'complete')
-            return true;
-        }
-        input.setAttribute('id', 'wrong');
-      
-         value = "";
-     
-      value.comment.focus();
-        return false;
-    }
+function readXML()
+{
+	var a = document.getElementById("download_link");
+	a.style.display = "none";
+	var add_button = document.getElementById("add_button");
+	add_button.style.display = "inline";
+	var xml = new XMLHttpRequest();
+	xml.open( 'GET', 'input.xml' , false );
+	xml.send();
+	var xmlData = xml.responseXML;
+	if( xmlData )
+	{
+		xmlData = ( new DOMParser() ).parseFromString( xml.responseText, 'text/xml' );
+		var params = xmlData.getElementsByTagName( "Parameter" );
+		console.log(params);
+		var output = '<tr><td>Id</td><td>Name</td><td>Description</td><td>Type</td></tr>';
+		for( var i = 0; i < params.length; i++ )
+		{
+			var type = identifyType( params[i].getElementsByTagName( "Type" )[0].textContent );
+			var value = params[i].getElementsByTagName( "Value" )[0].textContent;
+			var typeField = getFieldByType( type, value );
+			var idField = params[i].getElementsByTagName( "Id" )[0].textContent;
+			var nameField = params[i].getElementsByTagName( "Name" )[0].textContent;
+			var descriptionField = params[i].getElementsByTagName( "Description" )[0].textContent;
+			output += '<tr>';
+			output += '<td>' + "<input type=\"text\" value=\"" + idField + "\" />" + '</td>';
+			output += '<td>' + "<input type=\"text\" value=\"" + nameField + "\" />" + '</td>';
+			output += '<td>' + "<input type=\"text\" value=\"" + descriptionField + "\" />" + '</td>';
+			output += '<td>' + typeField + '</td>';
+			output += "<td><input type=\"button\" value=\"x\" onclick=\"deleteRow(this)\"></td>";
+			output += '</tr>';
+		}
+		document.getElementById( 'table_id' ).innerHTML = output;
+	}
 }
 
-function ReadInput() {
-    var input = new XMLHttpRequest();
-    input.open("GET", "Input.xml", false);
-    input.send();
-    var data = input.responseXML;
-    if (data) {
-        data = (new DOMParser()).parseFromString(input.responseText, 'text/xml');
-        var params = data.getElementsByTagName("Parameter");
-        console.log(params);
-        var display = '<tr><td id="title">Id</td><td id="title">Name</td><td id="title">Description</td><td id="title">Type</td></tr>';
-        for (var i = 0; i < params.length; i++) {
-            var type = initializeType(params[i].getElementsByTagName("Type")[0].textContent);
-            var value = params[i].getElementsByTagName("Value")[0].textContent;
-            var typeField = getFieldType(type, value);
-            var idField = params[i].getElementsByTagName("Id")[0].textContent;
-            var nameField = params[i].getElementsByTagName("Name")[0].textContent;
-            var descriptionField = params[i].getElementsByTagName("Description")[0].textContent;
-            display += '<tr>';
-            display += '<td>' + "<input type=\"text\" value=\"" + idField + "\" />" + '</td>';
-            display += '<td>' + "<input type=\"text\" value=\"" + nameField + "\" />" + '</td>';
-            display += '<td>' + "<input type=\"text\" value=\"" + descriptionField + "\" />" + '</td>';
-            display += '<td>' + typeField + '</td>';
-            display += "<td><input type=\"button\" id=\"DeleteButton\" value=\" Delete \"  onclick=\"deleteRow(this)\"></td>";
-            display += '</tr>';
-        }
-        document.getElementById('id_Table').innerHTML = display;
-    }
+function identifyType( str )
+{
+	if( str.indexOf('String') > -1 )
+	{
+		return 'String';
+	}
+	else if( str.indexOf('Int') > -1 )
+	{
+		return 'Int';
+	}
+	else if( str.indexOf('Boolean') > -1 )
+		return 'Boolean';
+	return '';
 }
 
-function initializeType(str) {
-    if (str.indexOf('String') > -1) {
-        return 'String';
-    }
-    else if (str.indexOf('Int') > -1) {
-        return 'Int';
-    }
-    else if (str.indexOf('Boolean') > -1)
-        return 'Boolean';
-    return '';
+function checkNumber( input ) 
+{	
+	var value = input.value;
+	var Reg = new RegExp("(^([+-]?)([1-9]+?)[0-9]*)|^0$");
+    if (Reg.test(value) && value <= MAX_INT_32) 
+	{
+		input.style.backgroundColor = '';
+		return true;
+	}
+	input.style.backgroundColor = 'red';
+	return false;
 }
 
-function getFieldType(type, value) {
-    switch (type) {
-        case 'String':
-            if (value === "")
-                return "<input type=\"text\"  onpaste=\"return false;\" onchange = \"validator(this)\" />";
-            return "<input type=\"text\"  onpaste=\"return false;\" onchange = \"validator(this,'System.String')\" value=" + value + " />";
-        case 'Int':
-            return "<input type=\"number\" onpaste=\"return false;\" onchange = \"validator(this,'System.Int32')\"  oninput = \"checkFieldForNumber(this)\"  value=" + value + " />";
-        case 'Boolean':
-        case 'Boolean':
-            {
-                var val_checkbox = "";
-                if (value === "True")
-                    val_checkbox = "checked";
-                return "<input type=\"checkbox\"" + val_checkbox + "/>";
-            }
-    }
+function onPaste(event, input)
+{	
+	var val = event.clipboardData.getData('Text');
+	if( input.value >= MAX_INT_32 )
+	{
+		return false;
+	}
+	return  (/^-?\d*$/.test(val));
 }
 
-function CreateRow() {
 
-    var table = document.getElementById('id_Table');
-    var newRow = table.insertRow(1);
-    for (var i = 0; i < CELL_Number; i++) {
-        var newColumn = newRow.insertCell(i);
-        newColumn.innerHTML = "<input type=\"text\" value=\"\" />";
-    }
-    var newColumn = newRow.insertCell(CELL_Number);
-    newColumn.innerHTML = " <select id=\"SelType\" size=\"3\" onchange=\"SelectChanging( this )\" ><option>String</option><option>Int</option><option>Boolean</option></select>"
-    newColumn = newRow.insertCell(CELL_Number + 1);
-    newColumn.innerHTML = " <input type=\"button\" id=\"DeleteButton\" value=\" Delete \"  onclick=\"deleteRow(this)\">"
+function onKeyUpEvent(event, input)
+{
+	if( event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 8 )
+		return;
+	if (input.getAttribute('value').length < 2) { 
+		input.setAttribute("value", input.value); 
+		return; 
+	} 
+	var regular = new RegExp("(^([+-]?)([1-9]+?)[0-9]*$)|^0$"); 
+	if (!regular.test(input.value)) { 
+		input.value = input.getAttribute('value'); 
+		return; 
+	} 
+	input.setAttribute("value", input.value);
+}
+function onKeyDownEvent(event, input)
+{
+	if( input.selectionStart == 0 && event.keyCode == 96 && input.value.length >= 1 )
+		return false;
+	if( input.selectionStart == 1 && event.keyCode == 96 && input.value.length >= 1 && ( input.value[0] == "-" || input.value[0] == "+") )
+		return false;
 }
 
-function SelectChanging(select) {
-    var table = document.getElementById('id_Table');
+function onKeyPressEvent( event, input )
+{
+	var res = true;
+	if(event.keyCode<48 || event.keyCode>57) 
+		res= false;
+	if(event.keyCode==45)
+	{
+		if( input.value.indexOf('-') == -1  && event.currentTarget.selectionEnd == 0 )
+			res = true;
+	}	
+	else if(event.keyCode==43)
+	{
+		if( input.value.indexOf('+') == -1  && event.currentTarget.selectionEnd == 0 )
+			res = true;
+	}	
+	if( res )
+	{
+		if( input.value.length+1 > MAX_CNT_INT )
+			res = false;
+	}
+	if( input.value.indexOf('0') == 0 )
+		input.value = "";
+	return res;
+}
+
+function getFieldByType( type, value )
+{
+	switch ( type ) 
+	{
+		case 'String':
+		if( value === "" )
+			return "<input type=\"text\" />";
+			return "<input type=\"text\" value=" + value + " />";
+		case 'Int':
+			return "<input type=\"text\" onpaste=\"return onPaste(event, this)\" digit=\"true\" onkeydown=\" return onKeyDownEvent(event, this)\" onkeyup=\" return onKeyUpEvent(event, this)\" onkeypress=\" return onKeyPressEvent(event, this)\" value=" + value + " />";
+		case 'Boolean':
+			var val_checkbox = "";
+			if( value === "True" )
+				val_checkbox = "checked";
+			return "<input type=\"checkbox\"" + val_checkbox + "/>";
+	}
+}
+
+function getDefaultValueByType( type )
+{
+	switch ( type ) 
+	{
+		case 'String':
+			return "";
+		case 'Int':
+			return 0;
+		case 'Boolean':
+			return "False";
+	}
+}
+
+function addRow()
+{
+	var a = document.getElementById("download_link");
+	a.style.display = "none";
+  
+	var	table =	document.getElementById( 'table_id' );
+	var newRow=table.insertRow(1);
+	for( var i = 0; i < cnstCNT_CELL; i++ )
+	{
+		var newCell = newRow.insertCell(i);
+		newCell.innerHTML="<input type=\"text\" value=\"\" />";
+	}
+	var newCell = newRow.insertCell( cnstCNT_CELL );
+	newCell.innerHTML= " <select id=\"SelType\" onchange=\"onChangeSelect( this )\" ><option>String</option><option>Int</option><option>Boolean</option></select>"
+	newCell = newRow.insertCell( cnstCNT_CELL+1 );
+	newCell.innerHTML= " <input type=\"button\" value=\"x\" onclick=\"deleteRow(this)\">"
+}
+
+function deleteRow(r)
+{
+	var i=r.parentNode.parentNode.rowIndex;
+	document.getElementById('table_id').deleteRow(i);
+}
+
+function onChangeSelect( select )
+{
+	var	table =	document.getElementById( 'table_id' );
     var allRows = table.getElementsByTagName("tr");
-    var i = select.parentNode.parentNode.rowIndex;
-    var cell = allRows[i].getElementsByTagName("td")[CELL_Number - 1];
-    var value = "";
-    switch (select.value) {
-        case 'String':
-            value = "";
-        case 'Int':
-            value = 0;
-        case 'Boolean':
-            value = "False";
-    }
-    cell.innerHTML = getFieldType(select.value, value);
+	var i=select.parentNode.parentNode.rowIndex;
+	var cell = allRows[i].getElementsByTagName("td")[cnstCNT_CELL-1];
+	cell.innerHTML = getFieldByType( select.value, getDefaultValueByType( select.value ) );
 }
 
-function deleteRow(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    document.getElementById('id_Table').deleteRow(i);
+function getXMLType( child )
+{
+	switch ( child.getAttribute('type') ) 
+	{
+		case 'text':
+		{
+		    if( child.getAttribute('digit') == "true") 
+				return "System.Int32";
+			else
+				return "System.String";
+		}
+		case 'checkbox':
+			return "System.Boolean";
+	}
 }
 
-function download(name, typeOfFile) {
-    var text = getOutputFile();
-    if (text === "")
-        return;
-    var link = document.createElement("a");
-    link.setAttribute('href', 'data:xml/plain;charset=utf-8,' + encodeURIComponent(text));
-    var file = new Blob([text], { type: typeOfFile });
-    link.setAttribute('download', name);
-    link.style.display = 'none';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+function getXMLValueType( child )
+{
+	if( child.type == "checkbox" && child.checked == true )
+		return "True";
+	if( child.type == "checkbox" && child.checked == false )
+		return "False";
+	
+	return child.value;
 }
 
-function getOutputFile() {
-    var res = "<?xml version='1.0' encoding='utf-8' ?> \n" +
-           "<Parameters xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema'> " + '\n';
-    var table = document.getElementById("id_Table");
-    var countRows = table.getElementsByTagName("tr");
-    res += "<Params>\n";
-    var isBool = false;
-    var isWrong = false;
-    for (var i = 1; i < countRows.length; i++) {
-        res += "<Param>\n"
-        var allColumns = countRows[i].getElementsByTagName("td");
-        res += "<Id>" + allColumns[0].childNodes[0].value + "</Id>\n";
-        res += "<Name>" + allColumns[1].childNodes[0].value + "</Name>\n";
+function getTableInXML()
+{
+	var	table =	document.getElementById( 'table_id' );
+    var allRows = table.getElementsByTagName("tr");
+	var outStr = "<?xml version=\"1.0\"?>\n";
+	outStr += "<Parameters>\n";
+	for( var i = 1; i < allRows.length; i++ )
+	{
+		outStr += "<Parameter>\n";
+		var allCell = allRows[i].getElementsByTagName("td");
+		outStr += "<Id>" + allCell[0].childNodes[0].value + "</Id>\n";
+		outStr += "<Name>" + allCell[1].childNodes[0].value + "</Name>\n";
+		outStr += "<Description>" + allCell[2].childNodes[0].value + "</Description>\n";
 
-        res += "<Description>" + allColumns[2].childNodes[0].value + "</Description>\n";
-        console.log(allColumns[3].childNodes[0].getAttribute('type'));
-        var type = "";
-        if (allColumns[3].childNodes[0].getAttribute('type') == 'number') {
-            type = "System.Int32";
-        }
-        else if (allColumns[3].childNodes[0].getAttribute('type') == 'text') {
-            var value = allColumns[3].childNodes[0].getAttribute('value');
-            value = allColumns[3].childNodes[0].value;
-            type = "System.String";
-        }
-        else if (allColumns[3].childNodes[0].getAttribute('type') == 'checkbox') {
-            allColumns[3].childNodes[0].setAttribute('id', 'checkbox');
-            type = "System.Boolean";
-            console.log(allColumns[3].childNodes[0].value);
-        }
-        if (allColumns[3].childNodes[0].value == "on")
-            res += "<Value>" + "True" + "</Value>\n";
-        else {
-            if (allColumns[3].childNodes[0].value == "off")
-                res += "<Value>" + "False" + "</Value>\n";
-            else {
-                res += "<Value>" + allColumns[3].childNodes[0].value + "</Value>\n";
-            }
-        }
-        if (type === 'System.Boolean') {
-            console.log("Znachenie Bool")
-            console.log(allColumns[3].childNodes[0].value);
-        }
-    if ((isValueValid(allColumns[3].childNodes[0], type) === true) && (type === 'System.Boolean')) {
-            allColumns[3].childNodes[0].setAttribute('id', 'complete');
-            isBool = true;
-        }
-    if ((isValueValid(allColumns[3].childNodes[0], type) === true)) {
-            switch (type) {
-                case 'System.String':
-                    {
-                        allColumns[3].childNodes[0].setAttribute('id', 'complete');
-                    }
-                case 'System.Int32':
-                    {
-                        allColumns[3].childNodes[0].setAttribute('id', 'complete');
-                    }
-            }
-        }
-     if ((isValueValid(allColumns[3].childNodes[0], type) === false))
-            switch (type) {
-            case 'System.String':
-                {
-                    allColumns[3].childNodes[0].setAttribute('id', 'wrong');
-                    alert('Количество символов  не может быть больше 10!');
-                    isWrong = true;
-                }
-            case 'System.Int32':
-                {
-                    allColumns[3].childNodes[0].setAttribute('id', 'wrong');
-                    alert('Введенное число больше максимального числа!');
-                    isWrong = true;
-                }
-            case 'System.Boolean':
-                {
-                }
-        }
-    }
-    if (!isBool) {
-        alert('Хотя бы 1 checkbox должен быть true!');
-        isWrong = true;
-    }
-    res += "</Param>";
-    res += "</Params>";
-    if (isWrong === true)
-        return "";
-    else {
-        console.log(res);
-        return res;
-    }
+		console.log( allCell[3].childNodes[0].getAttribute('type') );
+		var xmlType = getXMLType( allCell[3].childNodes[0] );
+		outStr += "<Type>" + xmlType + "</Type>\n";
+		if( xmlType == "System.Int32" && !checkNumber( allCell[3].childNodes[0] ) )
+			return "";
+		var xmlValue = getXMLValueType( allCell[3].childNodes[0] );
+		outStr += "<Value>" + xmlValue + "</Value>\n";
+		console.log( allCell[3].childNodes[0] );
+		outStr += "</Parameter>";
+	}
+	outStr += "</Parameters>";
+	return outStr;
 }
 
-function isValueValid(value, type) {
-    switch (type) {
-        case 'System.String':
-            {
-                if (value.value.length > 10) {
-
-                    return false;
-                }
-                return true;
-            }
-        case 'System.Int32':
-            {
-                if (value.value > 32768)
-                    return false;
-                if (value.value < -32768) {
-                    return false;
-                }
-                return true;
-            }
-        case 'System.Boolean':
-            {
-
-                console.log("value Bool");
-                if (value.checked === true) {
-                    console.log(true);
-                    return true;
-                }
-                else {
-                    console.log("Это False");
-                    return false;
-                }
-            }
-    }
+function checkTable()
+{
+	var	table =	document.getElementById( 'table_id' );
+    var allRows = table.getElementsByTagName("tr");
+	var boolExist = false;
+	for( var i = 1; i < allRows.length; i++ )
+	{
+		var allCell = allRows[i].getElementsByTagName("td");
+		var xmlType = getXMLType( allCell[3].childNodes[0] );
+		if( xmlType == "System.Boolean" )
+		{
+			boolExist = true;
+			var xmlValue = getXMLValueType( allCell[3].childNodes[0] );
+			if( xmlValue == "True" )
+				return true;
+		}
+	}
+	return boolExist ? false : true;
 }
 
-function checkFieldForNumber(field) { 
-  //  var index;
-    if (field.getAttribute('value').length < 1) {
-      field.setAttribute("value", field.value);
-     return;
-    }
-    var regular = new RegExp("(^([+-]?)([1-9]+?)[0-9]*$)|^0$");
-   // var regular = new RegExp("^([+-]?)[0-9]+$");
-    if (!regular.test(field.value)) {
-        field.value = field.getAttribute('value');
-        index = field.selectionStart;
-      //  field.setAttribute("value", field.value);
-        return;
-    }
-  //  
-    field.setAttribute("value", field.value);
-   //  field.selectionStart = index;
-//    field.selectionEnd = index-1;
- //if ('selectionStart' in field) {
-       //         field.selectionStart = index;
-         //       field.selectionEnd = index;
-         //       field.focus ();
-           // }
-    //
-// value.selectionStart = index;
-   //  value.selectionEnd = index-1;
-   //alert(index);
+function download( name, type) {
+  if( !checkTable() )
+  {
+	alert( "Fix error in table" );
+		return;
+  }
+  var text = getTableInXML();
+  if( text == "" )
+  {
+	alert( "Fix error in table" );
+	return;
+  }
+  var a = document.getElementById("download_link");
+  var file = new Blob([text], {type: type});
+  a.href = URL.createObjectURL(file);
+  a.download = name;
+  a.style.display = "inline";
+}
+
+function onChangeTable()
+{
+  var a = document.getElementById("download_link");
+  a.style.display = "none";
 }

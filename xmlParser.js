ed4f455d0cebb42b9c64329283fca3c8be9d31e5
petrovaelena <lena517594@yandex.ru>
2016-12-05 15:55:40 +0300

    Update xmlParser.js

--- xmlParser.js
+++ xmlParser.js
@@ -17,7 +17,7 @@ function readXML()
 		xmlData = ( new DOMParser() ).parseFromString( xml.responseText, 'text/xml' );
 		var params = xmlData.getElementsByTagName( "Parameter" );
 		console.log(params);
-		var output = '<tr><td>Ид</td><td>Name</td><td>Description</td><td>Type</td></tr>';
+		var output = '<tr><td>Ид</td><td>ФИО сотрудника</td><td>Название программы</td><td>Сдана</td></tr>';
 		for( var i = 0; i < params.length; i++ )
 		{
 			var type = identifyType( params[i].getElementsByTagName( "Type" )[0].textContent );
